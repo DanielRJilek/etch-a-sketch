62a0d30469b8b15ef011976c35d13f61fc582082
danielrjilek <jilek.d@northeastern.edu>
2025-05-13 14:14:17 -0500

    Hover effect implemented.

--- script.js
+++ script.js
@@ -3,27 +3,18 @@ const container = document.createElement("div");
 container.setAttribute("id", "container");
 body.appendChild(container);
 
-// container.style.display = "flex";
-// container.style.flexDirection = "column";
-// container.style.width = "75vw";
-// container.style.height = "75vw";
-// container.style.min
-
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div")
     row.classList.add("row");
     row.classList.add(i);
-    // row.style.height = "6.25%";
-    // row.style.minHeight = "6.25%";
-    // row.style.width = "100%";              
-    // row.style.display = "flex";
+    
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        // square.style.border = "solid black 2px";
+        
         square.classList.add("square")
-        // square.style.height = "100%";
-        // square.style.flex = "1";
-        // square.style.backgroundColor = "red";
+        square.addEventListener("mouseover", () => {
+            square.classList.add("hover");
+        });
         row.appendChild(square);
     }
     container.appendChild(row);
